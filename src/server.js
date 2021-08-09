2f1c0e39cd5e80239eb2b57aea90e60a241c9b92
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2021-08-09 19:53:25 -0300

    avançando no projeto

--- src/server.js
+++ src/server.js
@@ -1,4 +1,12 @@
 const express = require('express')
+const path = require('path')
+const route = require('./route')
 
 const server = express()
+
+server.set('view engine','ejs')
+
+server.set('views',path.join(__dirname,'views'))
+
+server.use(route)
 server.listen(3000,() => console.log('servidor iniciado com sucesso'))
