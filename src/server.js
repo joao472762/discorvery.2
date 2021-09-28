21b97060f790e83ca9494d2d906ea25fb24d0324
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2021-09-28 17:50:22 -0300

    commit init

--- src/server.js
+++ src/server.js
@@ -9,4 +9,6 @@ server.set('view engine','ejs')
 server.set('views',path.join(__dirname,'views'))
 
 server.use(route)
+server.use(express.static('public'))
+
 server.listen(3000,() => console.log('servidor iniciado com sucesso'))
